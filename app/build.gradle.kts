9b7e9a88b6d8e42ae1ce4f11b941c6badd0c1460
viplesoft <viplesoft@gmail.com>
2025-02-07 15:02:47 +0900

    Version up
    1. Remove FileProvider setting from AndroidManifest file
    2. insert android.enableJetifier=true in gradle.properties file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.12.0")
 
-    implementation("io.github.viplesoft:ccmosdk-light:0.9.10")
+    implementation("io.github.viplesoft:ccmosdk-light:0.9.21")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
